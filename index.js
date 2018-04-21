25b96ad6e048430af7946c49260993b326bb8f0f
Mike C <mikecronin92@gmail.com>
2018-04-21 23:50:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector) {
+  return document.querySelectorAll(selector)[0];
+}
+
+function nestedTarget() {
+  return document.querySelectorAll("#nested .target")[0];
+}
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div");
+}
+
+function increaseRankBy(n) {
+  
+}
