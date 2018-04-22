7008630ace247ccbda4bb949d4e0169dbc0f6604
Mike C <mikecronin92@gmail.com>
2018-04-22 00:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function increaseRankBy(n) {
   const lis = document.querySelectorAll('.ranked-list li')
  
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + n).toString()
+    let num = lis[i].innerHTML;
+    lis[i].innerHTML = num + n;
   }
 }
